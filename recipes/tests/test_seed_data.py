fd66cc6b15f7cf16ec3b5f3362f40004062060f5
AsadK47 <asadkhanruby@gmail.com>
2024-12-18 13:43:43 +0000

    Added test for seeding data

---
+++ recipes/tests/test_seed_data.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from recipes.models import Recipe, Ingredient
+from recipes.management.commands.seed_data import Command
+
+class SeedDataTest(TestCase):
+  def test_seed_data_creates_recipes_and_ingredients(self):
+    command = Command()
+    command.handle()
+
+    recipes = Recipe.objects.all()
+    ingredients = Ingredient.objects.all()
+
+    self.assertEqual(len(recipes), 2)
+    self.assertEqual(len(ingredients), 28)
+
+    for recipe in recipes:
+      self.assertGreater(len(recipe.ingredients.all()), 0)
